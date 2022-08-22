a8d6ab612accf07c57b3af25b7ea820b7708a584
smy <1316612932@qq.com>
2022-08-22 14:29:43 +0800

    element-ui组件库按需导入

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 按需引入 element-ui
+import { Button } from 'element-ui'
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 }
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
